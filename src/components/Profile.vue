9ec0ed6354dd43c9637820cdb9c0d95cde329b0f
mergimu <mergim.ujkani@gmail.com>
2021-03-03 16:21:03 +0100

    Profile UI updated

---
+++ src/components/Profile.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="profile">
+    <h1 class="profile__name"><span>Mergim</span> Ujkani</h1>
+    <div class="profile__banner"></div>
+    <div class="profile__image"></div>
+  </div>
+</template>
+
+<style lang="scss">
+  .profile {
+    background-color: #fff;
+    width: 100%;
+    max-width: 300px;
+    margin-left: auto;
+    position: relative;
+
+    &__name {
+      font-size: 24px;
+      font-weight: 700;
+      font-family: 'Raleway', sans-serif;
+      text-align: center;
+      padding-top: 40px;
+
+      span {
+        color: var(--brand-1);
+      }
+    }
+
+    &__image {
+      width: 90%;
+      height: 75%;
+      background-color: var(--brand-2);
+      position: absolute;
+      bottom: 10%;
+      left: 5%;
+
+      background: url('~@/assets/images/mergimujkani.jpg') no-repeat bottom left;
+    }
+
+    &__banner {
+      height: 270px;
+      width: 800px;
+      position: absolute;
+      bottom: 30%;
+      right: -40px;
+      background-color: var(--brand-1);
+    }
+  }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home__inner">
+      <div class="home__left">
+        <Profile/>
+      </div>
+      <div class="home__right"></div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+  import Profile from '../components/Profile';
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    name: 'home',
+    components: {
+      Profile,
+    },
+  };
+
 </script>
+
+<style lang="scss">
+  .home {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 30px;
+    
+    &__inner {
+      background-color: var(--brand-2);
+      width: 100%;
+      max-width: 1300px;
+      height: 100%;
+      max-height: 740px;
+      border-radius: 50px;
+      overflow: hidden;
+
+      &:before {
+        content: '';
+        display: block;
+        position: absolute;
+        top: 50%;
+        left: 0;
+        height: 60%;
+        width: 100%;
+        transform: translateY(-50%) rotate(-31deg) scaleX(2);
+        transform-origin: center center;
+        background-color: rgba(#000, 0.02);
+        z-index: -1;
+      }
+    }
+
+    &__left {
+      // background-color: cyan;
+      width: 100%;
+      max-width: 500px;
+      height: 100%;
+      display: flex;
+    }
+  }
+
+</style>
