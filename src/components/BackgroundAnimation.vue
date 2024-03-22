b63d196f50fdfd5dea19b3e0000e2cdac52c6d14
Mergim Ujkani <mergim.ujkani@gmail.com>
2024-03-22 10:37:38 +0100

    Added background animation and image
    Cursor moving effect

--- src/components/BackgroundAnimation.vue
+++ src/components/BackgroundAnimation.vue
@@ -1,46 +1,59 @@
+<script lang="ts">
+import { onMounted } from 'vue';
+
+
+export default {
+  name: 'Background Animation',
+  data() {
+    return {
+      mobileSize: 768,
+      bgOverlayPosition: {
+        x: 0,
+        y: 0
+      },
+      mouseMoveStarted: false
+    }
+  },
+
+  mounted() {
+    if (window.innerWidth >= this.mobileSize) {
+      window.addEventListener('mousemove', this.handleMouseMove)
+    }
+
+    window.addEventListener('resize', () => {
+      if (window.innerWidth >= this.mobileSize) {
+        window.addEventListener('mousemove', this.handleMouseMove)
+      } else {
+        window.removeEventListener('mousemove', this.handleMouseMove)
+        this.bgOverlayPosition.x = 0;
+        this.bgOverlayPosition.y = 0;
+      }
+    })
+  },
+
+  methods: {
+    handleMouseMove(e: any) {
+      setTimeout(() => {
+        this.bgOverlayPosition.x = e.clientX + window.scrollX
+        this.bgOverlayPosition.y = e.clientY + window.scrollY
+      }, this.mouseMoveStarted ? 60 : 0)
+      this.mouseMoveStarted = true;
+    },
+  },
+
+}
+
+</script>
+
 <template>
-  <div>
-    <img class="svg-anim-1"
-         src="@/assets/images/illustrations/anim-1.svg"
-         alt="Codepen"
-         title="Codepen">
-    <img class="svg-anim-2"
-         src="@/assets/images/illustrations/anim-2.svg"
-         alt="Codepen"
-         title="Codepen">
-    <img class="svg-anim-3"
-         src="@/assets/images/illustrations/anim-3.svg"
-         alt="Codepen"
-         title="Codepen">
+  <div class="background">
+    <!-- <div class="background__mobile">
+      <div class="background__mobile__bg-1"></div>
+      <div class="background__mobile__bg-2"></div>
+    </div> -->
+    <div v-show="mouseMoveStarted"
+         class="background__overlay-bg"
+         :style="{ left: (bgOverlayPosition.x - 100) + 'px', top: (bgOverlayPosition.y - 100) + 'px' }">
+    </div>
   </div>
 </template>
-
-<style lang="scss">
-.svg-anim-1,
-.svg-anim-2,
-.svg-anim-3 {
-  display: none;
-  position: fixed;
-  z-index: -1;
-  transition: .3s;
-}
-
-.svg-anim-1 {
-  top: 0;
-  left: 0;
-  opacity: 0.2;
-}
-
-.svg-anim-2 {
-  bottom: 2.5rem;
-  left: 10%;
-  opacity: 0.1;
-}
-
-.svg-anim-3 {
-  top: 50%;
-  right: 10%;
-  opacity: 0.1;
-  transform: rotate(45deg);
-}
-</style>
