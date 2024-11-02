1af196d4048fad607efc7a2b51dc1bac23f8ff52
Mergim Ujkani <mergim.ujkani@gmail.com>
2024-11-02 16:28:44 +0100

    Updated time zone
    Fixed time not updating every second

--- src/app/_components/TextAnimation.tsx
+++ src/app/_components/TextAnimation.tsx
@@ -14,33 +14,36 @@ const TextAnimation = () => {
   useEffect(() => {
     const updateTime = () => {
       const kosovoTimeZone = new Date().toLocaleString('en-US', {
-        timeZone: 'Europe/Belgrade',
-        timeStyle: 'long',
+        timeZone: 'Europe/Prague',
+        timeStyle: 'medium',
         hourCycle: 'h24',
       });
       setAnimatedText((prevText) => ({
         ...prevText,
-        text: `${kosovoTimeZone} - Kosovo, Prishtina`,
+        text: `${kosovoTimeZone} (CET) - Kosovo, Prishtina`,
       }));
     };
 
     updateTime();
+    setTimeout(() => {
+      setAnimatedText((prevText) => ({
+        ...prevText,
+        completedAnimation: true,
+      }));
+    }, 2000);
     const intervalId = setInterval(updateTime, 1000);
 
     return () => clearInterval(intervalId);
   }, []);
 
   useEffect(() => {
-    if (animatedText.text.length === animatedText.animatedTextResult.length)
-      return;
+    if (animatedText.text.length === animatedText.animatedTextResult.length) return;
 
     const animateTypingText = () => {
       if (animatedText.iterationValue < animatedText.text.length) {
         setAnimatedText((prevText) => ({
           ...prevText,
-          animatedTextResult:
-            prevText.animatedTextResult +
-            prevText.text.charAt(prevText.iterationValue),
+          animatedTextResult: prevText.animatedTextResult + prevText.text.charAt(prevText.iterationValue),
           iterationValue: prevText.iterationValue + 1,
         }));
       } else {
@@ -51,15 +54,13 @@ const TextAnimation = () => {
     const intervalId = setInterval(animateTypingText, animatedText.speed);
 
     return () => clearInterval(intervalId);
-  }, [animatedText]);
+  }, [animatedText.text]);
 
   return (
     <>
       <p className='text-animation__time'>
         <span className='text-animation__time__date-location'>
-          {animatedText.completedAnimation
-            ? animatedText.text
-            : animatedText.animatedTextResult}
+          {animatedText.completedAnimation ? animatedText.text : animatedText.animatedTextResult}
         </span>
         <span className='text-animation__time__blinker'></span>
       </p>
